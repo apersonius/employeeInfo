ec154b1944086e89bec4d91675b3d944ba6a9125
apersonius <personius12@gmail.com>
2023-04-09 11:32:58 -0500

    create manager prompts

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-//runs app
+const inquirer = require('inquirer');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const fs = require('fs');
+
+const managerData = () => {
+    return inquirer.createPromptModule([{
+        type: 'input',
+        name: 'name',
+        message: 'Who is the manager?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is their employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?',
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: 'What is their office number?',
+    },
+])
+.then(answers => {
+    console.log(answers);
+    const manager = new Manager(answers.name, answers.id, answers.office);
+    team.push(manager);
+    questionsPrompt();
+})
+}
+
+managerData();
